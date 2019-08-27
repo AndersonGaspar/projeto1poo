785f511f39a07efa3164758916a42cca05da268d
Anderson Gaspar <andersongasparm@gmail.com>
2019-08-27 08:49:18 -0300

    criado a classe batedeira

---
+++ src/main/java/poo/Batedeira.java
@@ -0,0 +1,23 @@
+package poo;
+
+public class Batedeira {
+
+    private boolean estadoBatedeira;
+    private int velocidade;
+
+    public boolean isEstadoBatedeira() {
+        return estadoBatedeira;
+    }
+
+    public void setEstadoBatedeira(boolean estadoBatedeira) {
+        this.estadoBatedeira = estadoBatedeira;
+    }
+
+    public int getVelocidade() {
+        return velocidade;
+    }
+
+    public void setVelocidade(int velocidade) {
+        this.velocidade = velocidade;
+    }
+}
